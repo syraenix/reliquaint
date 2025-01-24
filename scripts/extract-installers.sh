2d52fa2e592b5a626ceef515f088a56383e5550a
Derek Duncan <derek.b.duncan@gmail.com>
2025-01-23 20:18:35 -0500

    Add installer extraction script and update README

---
+++ scripts/extract-installers.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+set -e
+
+# check if game installers are available
+if [ ! -n "$(find ../installers -maxdepth 1 -name "*.exe")" ]; then
+   echo "Game installers are missing. Please add installers to the /installers directory."
+   
+   exit 1
+fi
+
+# create /games directory if it doesn't exist
+if [ ! -d "../games" ]; then
+    echo "Creating /games directory..."
+    
+    mkdir ../games
+fi
+
+# extract QFG1
+echo "Extracting QFG1 games files..."
+
+if [ ! -d "../games/qfg1-vga" ]; then
+    mkdir ../games/qfg1-vga
+fi
+
+innoextract --exclude-temp --silent --output-dir ../games/qfg1-vga ../installers/qfg1.exe
+
+if [ -d "../games/qfg1-ega" ]; then
+    rm -rf ../games/qfg1-ega
+fi
+
+mv ../games/qfg1-vga/EGA ../games/qfg1-ega
+
+# extract QFG2
+echo "Extracting QFG2 games files ..."
+
+if [ ! -d "../games/qfg2" ]; then
+    mkdir ../games/qfg2
+fi
+
+innoextract --exclude-temp --silent --output-dir ../games/qfg2 ../installers/qfg2.exe
+
+# extract QFG3
+echo "Extracting QFG3 game files..."
+
+if [ ! -d "../games/qfg3" ]; then
+    mkdir ../games/qfg3
+fi
+
+innoextract --exclude-temp --silent --output-dir ../games/qfg3 ../installers/qfg3.exe
+
+# extract QFG4
+echo "Extracting QFG4 games files..."
+if [ ! -d "../games/qfg4" ]; then
+    mkdir ../games/qfg4
+fi
+
+innoextract --exclude-temp --silent --output-dir ../games/qfg4 ../installers/qfg4.exe
